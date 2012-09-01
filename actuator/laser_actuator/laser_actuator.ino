dfea0541e4b5075b336ba0da583fbcf98759eb6a
Josep <apuratepp@gmail.com>
2012-09-01 14:13:13 +0200

    espanta 0, 90 i 180 cada 1 s quan detecta moviment

--- actuator/laser_actuator/laser_actuator.ino
+++ actuator/laser_actuator/laser_actuator.ino
@@ -1,19 +1,43 @@
 /* 
-  laser_actuator 
-  Creat:     2012-08-29 (Josep)
-  Modificat: 2012-09-01 (Josep)
-
-
+ laser_actuator 
+ Creat:     2012-08-29 (Josep)
+ Modificat: 2012-09-01 (Josep)
 */
 
+#include <Servo.h>
+
 int led = 13;
+int motionPin = 2;
+int actuatorPin = 9;
+
+Servo actuator;
 
 void setup() {
- pinMode(led, OUTPUT); 
+  pinMode(led, OUTPUT);
+  pinMode(motionPin, INPUT);
+  actuator.attach(actuatorPin);
 }
 void loop() {
-  digitalWrite(led, HIGH);
+  int motionVal = digitalRead(motionPin);
+  if (motionVal == HIGH) {
+    digitalWrite(led, HIGH);
+    espanta();
+  }else{
+    digitalWrite(led, LOW);
+  }
+}
+
+
+/*
+  Funcio espanta()
+  Posa el laser a 0º, 90º, 18º0 i torna a 0º en intervals d' 1 segon
+*/
+void espanta(){
+  actuator.write(0);
   delay(1000);
-  digitalWrite(led, LOW);
+  actuator.write(90);
   delay(1000);
+  actuator.write(180);
+  delay(1000);
+  actuator.write(0);
 }
