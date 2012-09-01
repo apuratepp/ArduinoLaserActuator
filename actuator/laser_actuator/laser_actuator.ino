f904614b4da10cca7560cb9b17232be565893493
Josep <apuratepp@gmail.com>
2012-09-01 12:29:26 +0200

    testing leds

--- actuator/laser_actuator/laser_actuator.ino
+++ actuator/laser_actuator/laser_actuator.ino
@@ -1,8 +1,18 @@
 /* 
   laser_actuator 
-  Creat:     2012-08-29 (Josep)  
-  Modificat: 2012-09-01 (Josep) 
-  
-  
+  Creat:     2012-08-29 (Josep)
+  Modificat: 2012-09-01 (Josep)
+
 */
 
+int led = 13;
+
+void setup() {
+ pinMode(led, OUTPUT); 
+}
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+}
